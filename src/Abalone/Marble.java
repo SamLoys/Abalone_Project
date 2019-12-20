763f06fdb4a90e1214dbe9135e022a6a43da0e35
Sam <Sam@LAPTOP-IIS5AM1R>
2019-12-20 14:59:18 +0100

    Marbles

--- src/Abalone/Marble.java
+++ src/Abalone/Marble.java
@@ -1,8 +1,36 @@
 package Abalone;
 
-public enum Marble {
+
+public class Marble {
+	public enum states {
+		Empty, White, Black, Red, Blue, Death;
+	}
+
+	private states currentState;
+	private int amountPlayers; 
 	
-	Empty, White, Black, Red, Blue, Death; 
-	//nu wel?
+	/**
+	 * Is the constructor of the marble class
+	 * Sets the state to the given state, and stores the amount of players
+	 * 
+	 * @param s
+	 * @param i
+	 * 
+	 */
+	public Marble(states s, int i) {
+		this.currentState = s; 
+		this.amountPlayers = i; 
+	}
+	
+	public states getState() {
+		return this.currentState; 
+	}
+	
+	public void setState(states s) {
+		this.currentState = s; 
+	}
+	
+	
+
 	
 }

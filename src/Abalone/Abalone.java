c7b95008b1b09037f26d80972b10c74657feba86
Sam <Sam@LAPTOP-IIS5AM1R>
2019-12-20 14:00:38 +0100

    Deel

--- src/Abalone/Abalone.java
+++ src/Abalone/Abalone.java
@@ -1,5 +1,13 @@
 package Abalone;
 
 public class Abalone {
-
+	public static void main(String[] args) {
+		
+		
+		//create player
+		
+		
+		Game game = new Game(); 
+		//game.start
+	}
 }
--- src/Abalone/Board.java
+++ src/Abalone/Board.java
@@ -2,4 +2,39 @@ package Abalone;
 
 public class Board {
 
+	private Marble[][] fields;
+
+	public Board(int players) {
+		if (players == 2) {
+			initBoard2();
+		}
+
+		if (players == 3) {
+			initBoard3();
+		}
+
+		if (players == 4) {
+			initBoard4();
+		}
+		else {System.out.println("NotValid");}
+
+	}
+	
+	private void initBoard2() {
+		fields = new Marble[11][11]; 
+		
+	}
+	
+	private void initBoard3() {
+		fields = new Marble[11][11]; 
+		
+	}
+	
+	private void initBoard4() {
+		fields = new Marble[11][11]; 
+		
+	}
+	
+	
+
 }

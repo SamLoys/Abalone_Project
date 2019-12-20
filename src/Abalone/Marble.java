5c4b1499b9142d760f757e0d62f9277e49c750db
Sam <Sam@LAPTOP-IIS5AM1R>
2019-12-20 12:57:35 +0100

    pussh

--- src/Abalone/Marble.java
+++ src/Abalone/Marble.java
@@ -1,6 +1,28 @@
 package Abalone;
 
+import ss.week4.tictactoe.Mark;
+
 public enum Marble {
 
-	Empty, White, Black, Green, Blue, Death
+	Empty, White, Black, Green, Blue, Death; 
+	
+	
+	public Marble team() {
+		if (this == White) {
+			return Black; 
+		}
+		
+		if (this == White) {
+			return Black; 
+		}
+		
+		if (this == White) {
+			return Black; 
+		}
+		
+		if (this == White) {
+			return Black; 
+		}
+	}
+	
 }
